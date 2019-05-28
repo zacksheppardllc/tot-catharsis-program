aa06de5052cd99e3baf9ef2a5bff326c10826969
zackzachariah <zack@zacksheppard.com>
2019-05-28 03:09:38 +0000

    Remove the relative path gatsby markdown plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,19 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
-          {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads"
-            }
-          },
+          // {
+          //   resolve: "gatsby-remark-relative-images",
+          //   options: {
+          //     name: "img"
+          //   }
+          // },
           {
             resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048
+              maxWidth: 1800
             }
           },
           {
@@ -68,18 +68,5 @@ module.exports = {
       }
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify" // make sure to keep it last in the array
-  ],
-  // for avoiding CORS while developing Netlify Functions locally
-  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
-    app.use(
-      "/.netlify/functions/",
-      proxy({
-        target: "http://localhost:9000",
-        pathRewrite: {
-          "/.netlify/functions/": ""
-        }
-      })
-    );
-  }
+  ]
 };
